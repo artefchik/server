731d747b4a100494df2f6306eb5a44bc1af4db67
Artem Adeev <artemadeev42@gmail.com>
2024-03-22 09:20:48 +0300

    fix

--- build/src/routes/user.router.js
+++ build/src/routes/user.router.js
@@ -6,11 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 const express_validator_1 = require("express-validator");
 const user_controller_1 = __importDefault(require("../user/user.controller"));
-const upload_middleware_1 = require("../middleware/upload.middleware");
 // @ts-ignore
 const router = new express_1.Router();
-const multer_1 = __importDefault(require("multer"));
-const upload = (0, multer_1.default)(upload_middleware_1.multerConfig);
 router.post('/registration', (0, express_validator_1.body)('email').isEmail(), (0, express_validator_1.body)('password').isLength({ min: 4, max: 32 }), user_controller_1.default.registration);
 router.post('/login', user_controller_1.default.login);
 router.post('/logout', user_controller_1.default.logout);
